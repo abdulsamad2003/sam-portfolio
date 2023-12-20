2100d0d5d0b3f6187e93dd4e8bc2f9160b5f45ae
abdulsamad2003 <125445996+abdulsamad2003@users.noreply.github.com>
2023-12-20 20:45:41 +0530

    Update portfolio.js
    adding canvas tag functionality

--- public/portfolio.js
+++ public/portfolio.js
@@ -72,3 +72,97 @@ function show(anything) {
   });
 }
 // drop-down selecting category ends
+
+
+
+const canvas = document.querySelector("Canvas");
+const ctx = canvas.getContext("2d");
+const PARTICLES_ARRAY = [];
+const COLORS = [
+  "#20d9dffa",
+  "#20d9dffa"
+];
+const SPEEDS = [0.2, -0.2, 0.3, -0.3, 0.4, -0.4, 1, -1];
+const SIZES = [2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8]
+
+// function definitions
+function main() {
+  setCanvasDimensions();
+  window.addEventListener("resize", setCanvasDimensions);
+  animate();
+}
+
+function setCanvasDimensions() {
+  const contact = document.getElementById("profile");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+
+function handleParticles() {
+  for (let i = 0; i < PARTICLES_ARRAY.length; i++) {
+    PARTICLES_ARRAY[i].update();
+    PARTICLES_ARRAY[i].draw();
+  }
+}
+
+// class definitions
+class Particle {
+  constructor() {
+    this.x = Math.random() * window.innerWidth + 1;
+    this.y = Math.random() * window.innerHeight + 1;
+    this.size = SIZES[Math.floor(Math.random() * (SIZES.length - 1)) + 1];
+    this.speedX = SPEEDS[Math.floor(Math.random() * (SPEEDS.length - 1)) + 1];
+    this.speedY = SPEEDS[Math.floor(Math.random() * (SPEEDS.length - 1)) + 1];
+    this.color = COLORS[Math.floor(Math.random() * (COLORS.length - 1)) + 1];
+  }
+
+  update() {
+    if (this.x < 1) {
+      this.speedX = Math.max(this.speedX, -this.speedX);
+      this.x += this.speedX;
+    } else {
+      this.x += this.speedX;
+    }
+
+    if (this.y < 1) {
+      this.speedY = Math.max(this.speedY, -this.speedY);
+      this.y += this.speedY;
+    } else {
+      this.y += this.speedY;
+    }
+
+    if (this.x > window.innerWidth - 1) {
+      this.speedX = Math.min(this.speedX, -this.speedX);
+      this.x += this.speedX;
+    } else {
+      this.x += this.speedX;
+    }
+
+    if (this.y > window.innerHeight - 1) {
+      this.speedY = Math.min(this.speedY, -this.speedY);
+      this.y += this.speedY;
+    } else {
+      this.y += this.speedY;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+for (let i = 0; i < 180; i++) {
+  PARTICLES_ARRAY.push(new Particle());
+}
+
+main();
+
